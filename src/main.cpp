422d7dcdb39eaeae57563f131a37021228f0969e
landingpad75 <skydevsmc@outlook.com>
2026-01-09 18:24:56 +0100

    added optimization flags

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,25 @@
 #include <ecxp.hpp>
+#include <optimizer/optimizer.hpp>
 #include <iostream>
 
 struct GlobalConfig {
     bool debug = false;
+    bool optimize = true;
 };
 
 void setup(Environment& env);
 
 int main(int argc, char* argv[]){
     Console::Async::start();
-//    GlobalConfig conf;
+    GlobalConfig conf;
     ArgumentParser args(argc, argv);
 
     if(args.hasFlag("-h") || args.hasFlag( "--help")) {
         Console::Async::log("Usage: " + std::string(argv[0]) + " [files...]\n");
         Console::Async::log("Options:\n");
-        Console::Async::log("  -h, --help    Show this help message\n");
-        Console::Async::log("  -d, --debug   Enable debug mode\n");
+        Console::Async::log("  -h, --help       Show this help message\n");
+        Console::Async::log("  -d, --debug      Enable debug mode\n");
+        Console::Async::log("  -O0, --no-opt    Disable optimizations\n");
         Console::Async::stop();
         return 0;
     }
@@ -25,14 +28,25 @@ int main(int argc, char* argv[]){
         Console::Async::stop();
         return 1;
     }
+
+    if(args.hasFlag("-O0") || args.hasFlag("--no-opt")) {
+        conf.optimize = false;
+    }
+
     Parser parser;
     Interpreter irpr;
+    Optimizer optimizer;
     Environment env(nullptr);
     setup(env);
 
     for(auto& fil : args.getPositionalArgs()){
         std::string file = Utilities::readFile(fil);
         std::shared_ptr<AST::ExprAST> prog = parser.produceAST(file);
+
+        if(conf.optimize) {
+            prog = optimizer.optimize(prog);
+        }
+
         irpr.evaluate(prog, env);
     }
     Console::Async::stop();
