2ffc9a1a0a48adbb56b63c1eeb5940f13b552ac5
landingpad75 <skydevsmc@outlook.com>
2026-01-09 18:24:31 +0100

    fixed identifier error

--- src/executor/expressions/ident.cpp
+++ src/executor/expressions/ident.cpp
@@ -4,5 +4,10 @@ using string = std::string;
 
 std::unique_ptr<Values::Runtime> Interpreter::evaluateIdentifier(const AST::IdentifierExpr& node, Environment& env){
 	auto* hi = env.get(node.name);
+
+	if(hi == nullptr){
+		std::cout << "Couldn't find '" << node.name << "'\n";
+		exit(0);
+	}
 	return hi->clone();
 }
